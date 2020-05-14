50bf7680de4e064a15ac5188fe59912cd0c793a9
Raphael Barone <rpbarone.dev@gmail.com>
2020-05-14 18:11:41 -0300

    update our menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
-import './App.css';
+
 
 function App() {
     return (
     <div className='App'>
-      <HomePage />
+          <Switch>
+          <Route exact path='/' component={HomePage} />
+          </Switch>
     </div>
     );
   }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -13,44 +13,52 @@ class Directory extends React.Component {
                 {
                     title: 'banho de ervas',
                     imageUrl: 'https://i.ibb.co/4J1SK7t/banhos-de-ervas.jpg',
-                    id: 1
+                    id: 1,
+                    linkUrl: 'banhos-de-ervas'
                 },
                 {
                     title: 'fitoflorais',
                     imageUrl: 'https://i.ibb.co/2MmJPvn/fitoflorais.png',
-                    id: 2
+                    id: 2,
+                    linkUrl: 'fitoflorais'
                 },
                 {
                     title: 'elixir de cristais',
                     imageUrl: 'https://i.ibb.co/hcCs7GL/elixir-de-cristais.png',
-                    id: 3
+                    id: 3,
+                    linkUrl: 'elixir-de-cristais'
                 },
                 {
                     title: 'aromatizadores de ambiente',
                     imageUrl: 'https://i.ibb.co/MMHQGLB/aromatizadores.jpg',
-                    id: 4
+                    id: 4,
+                    linkUrl: 'aromatizadores'
                 },
                 {
                     title: 'gel de ervas',
                     imageUrl: 'https://i.ibb.co/sJDZT6j/gel-de-ervas.jpg',
-                    id: 5
+                    id: 5,
+                    linkUrl: 'gel-de-ervas'
                 },
                 {
                     title: 'óleo de ervas',
                     imageUrl: 'https://i.ibb.co/5rNpLyB/oleos-de-ervas.jpg',
-                    id: 6
+                    id: 6,
+                    linkUrl: 'oleos-de-ervas'
                 },
                 {
                     title: 'sabonetes dos orixás',
                     imageUrl: 'https://i.ibb.co/1nTRZ3N/sabonetes.jpg',
                     id: 7,
-                    size: 'large'
+                    size: 'large',
+                    linkUrl: 'sabonetes-dos-orixas'
                 },
                 {
                     title: 'pulseiras',
                     imageUrl: 'https://i.ibb.co/HYVdCpX/pulseiras.png',
                     id: 8,
-                    size: 'large'
+                    size: 'large',
+                    linkUrl: 'pulseiras'
                 },
             ]
         }
@@ -59,8 +67,8 @@ class Directory extends React.Component {
     render() {
         return(
             <div className='directory-menu'>
-                {this.state.sections.map(({title, imageUrl, id, size}) => (
-                    <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                {this.state.sections.map(({id, ...otherSectionProps}) => (
+                    <MenuItem key={id} {...otherSectionProps} />
                 ))}
             </div>
         )
